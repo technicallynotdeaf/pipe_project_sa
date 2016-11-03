993a16bb0466b507ef933a9fc7df1aa2ee9e094b
Alison Keen <ali.keen@gmail.com>
2016-11-03 06:49:54 +0000

    Initial commit so I don't accidentally delete today's work...

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PIPE Project Alpha</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <?php include 'navbar.php'; ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Jumbotron Header -->
+        <header class="jumbotron hero-spacer">
+            <h1>PIPE: Parliament in Plain English</h1>
+            <p> Increasing visibility of parliamentary proceedings. </p>
+            <!-- <p><a class="btn btn-primary btn-large">Call to action!</a> --> 
+            </p>
+        </header>
+
+        <hr>
+
+        <!-- Title -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h3>Coming early 2017</h3>
+            </div>
+        </div>
+        <!-- /.row -->
+
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ lib/xml_file_downloader.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env/ruby
+
+require 'csv'
+#require 'ostruct'
+require 'enumerator'
+
+
+class HansardCSVReader
+  
+  def HansardCSVReader.read_raw_csv(filename)
+    
+
+    if File.exists?(filename)
+      data = CSV.readlines(filename) 
+    else 
+      puts "File "+filename+" doesn't exist"
+      
+    end
+
+    # Ignores comment lines starting with '#'
+    data.delete_if {|line| line[0] && line[0][0..0] == '#'}
+
+    data
+  end
+  
+  def HansardCSVReader.read_index(filename)
+    data = read_raw_csv(filename)
+    data.shift # what does this do?
+    data.shift
+
+    data.each do |line|
+      date, url, house_of_parl = line[0..2]
+      
+      filename = ""
+
+      case (house_of_parl)
+      when "House of Assembly"
+        filename = "xmldata/lower/"
+      when "Legislative Council"
+        filename = "xmldata/upper/" 
+      else
+        filename = "xmldata/unknown-"
+      end
+
+      filename = filename + date + ".xml"
+
+      puts "Source: " + date + ", " + house_of_parl
+      puts "Output file:" + filename
+      puts "URL: " + url
+
+      #now attempt to download it... 
+      `curl --output #{filename} #{url}`
+    end
+
+  end
+  
+end
+
+reader = HansardCSVReader.new
+
+csvfilename = "Hansard_Source_CSV.csv"
+
+HansardCSVReader.read_index(csvfilename)
+
+
+
+SA_Hansard_index.csv
